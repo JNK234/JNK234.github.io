b0a50f879795afe1ad999083846eb36c530f35dc
JNK234 <42378047+JNK234@users.noreply.github.com>
2025-08-31 14:39:32 +0000

    Deploying to gh-pages from @ JNK234/jnkarthik-portfolio@a5015954f817fe195ce6018406ae300676935401 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "",
+    "data-repo": "JNK234/jnkarthik-portfolio",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
